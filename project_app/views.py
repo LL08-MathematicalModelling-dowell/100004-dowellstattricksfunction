a4403b704aa1d8eb82619d18c6cce506eca79772
Sohaibstylo <sohaibstylo03332140546@gmail.com>
2021-05-11 13:33:37 -0700

    maintainance

--- project_app/views.py
+++ project_app/views.py
@@ -16,7 +16,7 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
-client = pymongo.MongoClient("mongodb+srv://sohaib:sohaib2140@project0.ghjdn.mongodb.net/FB?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 db = client['mongodb']
 
 def default(request):
@@ -64,7 +64,7 @@ def stdev(data):
     return std_dev
 
 def insertData(request):
-    cursor=db.qr
+    cursor=db.QR_IMAGE
     if request.method=="POST":
         processId = request.POST.get("processId")
         processSequenceId = request.POST.get("processSequenceId")
@@ -103,10 +103,7 @@ def insertData(request):
         if result:
             return HttpResponse("Entered sequence id is already exist!")
         else:
-            cursor.insert_one({"title":"userData","processId":processId,"processSequenceId":processSequenceId,"series":series,"minimumSeries":minimumSeries,"minimumSeriesDatapoint":minimumSeriesDatapoint,"minimumContinuousDatapoint":minimumContinuousDatapoint,"processHours":processHours,"startTricks":startTricks,"mean":seriesValuesMean,"median":seriesValuesMedian,"mode":seriesValuesMode,"standardDeviation":stdValue,"moment1":moment1,"moment2":moment2,"moment3":moment3,"moment4":moment4})
-            print("done")
-            sendData={"title":"userData","processId":processId,"processSequenceId":processSequenceId,"series":series,"minimumSeries":minimumSeries,"minimumSeriesDatapoint":minimumSeriesDatapoint,"minimumContinuousDatapoint":minimumContinuousDatapoint,"processHours":processHours,"startTricks":startTricks,"mean":seriesValuesMean,"median":seriesValuesMedian,"mode":seriesValuesMode,"standardDeviation":stdValue,"moment1":moment1,"moment2":moment2,"moment3":moment3,"moment4":moment4}
-            insert(sendData)
+            cursor.insert_one({"document":"qr","title":"userData","processId":processId,"processSequenceId":processSequenceId,"series":series,"minimumSeries":minimumSeries,"minimumSeriesDatapoint":minimumSeriesDatapoint,"minimumContinuousDatapoint":minimumContinuousDatapoint,"processHours":processHours,"startTricks":startTricks,"mean":seriesValuesMean,"median":seriesValuesMedian,"mode":seriesValuesMode,"standardDeviation":stdValue,"moment1":moment1,"moment2":moment2,"moment3":moment3,"moment4":moment4})
             return HttpResponse("Inserted!")
     else:
         return HttpResponse("No Data Found")
@@ -114,9 +111,8 @@ def insertData(request):
 def fetchDataForm(request):
     return render(request,"fetchData.html")
 
-
 def fetchData(request):
-    cursor=db.qr
+    cursor=db.QR_IMAGE
     if request.method=="POST":
         processSequenceId = request.POST.get("psi")
         result=cursor.find_one({"processSequenceId":processSequenceId})
@@ -126,58 +122,3 @@ def fetchData(request):
             return HttpResponse(data)
         else:
             return HttpResponse("No Data Found")
-
-# def processSeries10001(request):
-#     cursor=db.qr
-#     result=cursor.find({"title":"userData"})
-#     temp=0
-#     minimumValue=0
-#     maximumValue=0
-#     series10001=[]
-#     for row in result:
-#         if temp==0:
-#             minimumValue=int(row["series"]["10001"])
-#             maximumValue=int(row["series"]["10001"])
-#             series10001.append(int(row["series"]["10001"]))
-#         else:
-#             if minimumValue > int(row["series"]["10001"]):
-#                 minimumValue = int(row["series"]["10001"])
-#             else:
-#                 pass
-#             if maximumValue < int(row["series"]["10001"]):
-#                 maximumValue=int(row["series"]["10001"])
-#             else:
-#                 pass
-#             series10001.append(int(row["series"]["10001"]))
-#         temp=temp+1
-#     meanValue=mean(series10001)
-#     medianValue=median(series10001)
-#     modeValue=mode(series10001)
-#     stdValue=stdev(series10001)
-#     moment1=moment(series10001, moment=1)
-#     moment2=moment(series10001, moment=2)
-#     moment3=moment(series10001, moment=3)
-#     moment4=moment(series10001, moment=4)
-#     cursor.find_one_and_update({"title":"processSeries10001"},{"$set":{"minimumValue":minimumValue,"mean":meanValue,"maximumValue":maximumValue,"median":medianValue,"mode":modeValue,"standardDeviation":str("%.3f" % stdValue),"m1":str(moment1),"m2":str(moment2),"m3":str(moment3),"m4":str(moment4)}})
-#     return HttpResponse("Minimum Value of Series 10001: "+str(minimumValue)+"<br>Maximum Value of Series 10001: "+str(maximumValue)+"<br>Mean Value of Series 10001: "+str("%.2f" % meanValue)+"<br>Median Value of Series 10001: "+str(medianValue)+"<br>Mode Value of Series 10001: "+str(modeValue[0])+"<br>Standard Deviation of Series 10001: "+str("%.3f" % stdValue)+"<br>m1 : "+str(moment1)+"<br>m2 : "+str(moment2)+"<br>m3 : "+str(moment3)+"<br>m4 : "+str(moment4))
-
-# def processAllDatapoints(request):
-#     cursor=db.qr
-#     result=cursor.find({"title":"userData"})
-#     dataPoints=[]
-#     for row in result:
-#         for i in row["series"]:
-#             dataPoints.append(int(row["series"][i]))
-#     kurtosisValue=kurtosis(dataPoints, fisher = True)
-#     meanValue = mean(dataPoints)
-#     medianValue = median(dataPoints)
-#     stdValue = stdev(dataPoints)
-#     moment1=moment(dataPoints, moment=1)
-#     moment2=moment(dataPoints, moment=2)
-#     moment3=moment(dataPoints, moment=3)
-#     moment4=moment(dataPoints, moment=4)
-#     temp1 = meanValue-medianValue
-#     temp1 = 3*temp1
-#     skewness=temp1/stdValue
-#     cursor.find_one_and_update({"title":"processAllDatapoints"},{"$set":{"kurtosis":kurtosisValue,"skewness":str("%.3f" %skewness),"m1":str(moment1),"m2":str(moment2),"m3":str(moment3),"m4":str(moment4)}})
-#     return HttpResponse("Kurtosis Value: "+str("%.3f" % kurtosisValue)+"<br>Skewness Value: "+str("%.3f" %skewness)+"<br>m1 : "+str(moment1)+"<br>m2 : "+str(moment2)+"<br>m3 : "+str(moment3)+"<br>m4 : "+str(moment4))
